44ebaf0ced4093ea7bca481f4a431eb47b16d966
GabMan03 <gabriele.maniaci@gmail.com>
2022-11-14 16:27:52 +0100

    update file sql

--- ProgettoTVPD/SQL/create_obj.sql
+++ ProgettoTVPD/SQL/create_obj.sql
@@ -14,16 +14,17 @@ create table docente(
 );
 
 create table corso(
-	codCorso int,
-	constraint p_codCorso primary key(codCorso),
-	nomeCorso varchar2(30) not null,
-	dataInizioCorso date not null,
-	dataFineCorso date not null check datediff(day, dataFineCorso, dataInizioCorso)>1,
-	costoCorso double(7,2) not null,
-	commentiCorso varchar2(30) default 'nessun commento',
-	aulaCorso varchar2(30) not null,
-	codDocente int,
-	constraint f_codDocente foreign key(codDocente) references docente(codDocente) on delete cascade
+codCorso int,
+constraint p_codCorso primary key(codCorso),
+nomeCorso varchar2(30) not null,
+dataInizioCorso date not null,
+dataFineCorso date not null,
+costoCorso number(7,2) not null,
+commentiCorso varchar2(30) default 'nessun commento',
+aulaCorso varchar2(30) not null,
+codDocente int,
+constraint f_codDocente foreign key(codDocente) references docente(codDocente),
+check ((dataFineCorso-1)>dataInizioCorso)
 );
 	
 
@@ -32,15 +33,15 @@ create table corsista(
 	constraint p_codCorsista primary key(codCorsista),
 	nomeCorsista varchar2(30) not null,
 	cognomeCorsista varchar2(30) not null,
-	precedentiFormativi number(1) not null,
+	precedentiFormativi number(1) not null
 );
 
 create table corso_Corsista(
-	codCorso int,
-	codCorsista int,
-	constraint f_codCorso foreign key(codCorso) references corso(codCorso) on delete cascade,
-	constraint f_codCorsista foreign key(codCorsista) references corsista(codCorsista) on delete cascade,
-	constraint p_corso_corsista primary key(codCorso, codCorsista)
+codCorso int,
+codCorsista int,
+constraint f_codCorso foreign key(codCorso) references corso(codCorso),
+constraint f_codCorsista foreign key(codCorsista) references corsista(codCorsista),
+constraint p_corso_corsista primary key(codCorso, codCorsista)
 );
 
 create sequence corso_seq;
--- ProgettoTVPD/SQL/delete_obj.sql
+++ ProgettoTVPD/SQL/delete_obj.sql
@@ -5,6 +5,7 @@ drop table docente cascade constraint;
 drop table corso cascade constraint;
 drop table corsista cascade constraint;
 drop table corso_corsista cascade constraint;
+drop view corsiPerDocente;
 
 -- Delete sequence:
 
