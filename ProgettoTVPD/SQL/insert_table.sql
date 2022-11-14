7c05a541b53e9e471bdf44f1f67935353ecf743c
Zanck-03 <m.zanchetta7@gmail.com>
2022-11-14 16:52:26 +0100

    fix table

--- ProgettoTVPD/SQL/insert_table.sql
+++ ProgettoTVPD/SQL/insert_table.sql
@@ -8,5 +8,5 @@ insert into amministratore values('A0002', 'admin2', 'Pass02$');
 insert into docente values(1, 'Marco', 'Zanchetta','MarcoZanchetta.pdf');
 insert into docente values(2, 'Gabriele', 'Maniaci','GabrieleManiaci.pdf');
 insert into docente values(3, 'GianBattista', 'Lorenzato', 'GianBattistaLorenzato.pdf');
-insert into docente values(4, 'Lucas', 'Teixeira', 'LucasTeixeira');
-insert into docente values(5, 'Alvise', 'Battistich', 'AlviseBattistich');
+insert into docente values(4, 'Lucas', 'Teixeira', 'LucasTeixeira.pdf');
+insert into docente values(5, 'Alvise', 'Battistich', 'AlviseBattistich.pdf');
