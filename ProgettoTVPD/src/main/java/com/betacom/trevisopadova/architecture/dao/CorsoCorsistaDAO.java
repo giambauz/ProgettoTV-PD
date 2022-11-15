ac2ab3674cdd509a324a9f24a1115827abf56c05
giambauz <lorenzatogb@gmail.com>
2022-11-15 09:45:16 +0100

    CorsoCorsistaDAO concluso e sistemato il BC

--- ProgettoTVPD/src/main/java/com/betacom/trevisopadova/architecture/dao/CorsoCorsistaDAO.java
+++ ProgettoTVPD/src/main/java/com/betacom/trevisopadova/architecture/dao/CorsoCorsistaDAO.java
@@ -1,12 +1,13 @@
 package com.betacom.trevisopadova.architecture.dao;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.sql.rowset.CachedRowSet;
 
 import com.betacom.trevisopadova.businesscomponent.model.CorsoCorsista;
 
-public class CorsoCorsistaDAO { //TODO implements DAOConstats
+public class CorsoCorsistaDAO implements DAOConstants{ 
 	private CachedRowSet rowSet;
 	
 	private CorsoCorsistaDAO() {
@@ -17,7 +18,14 @@ public class CorsoCorsistaDAO { //TODO implements DAOConstats
 		return new CorsoCorsistaDAO();
 	}
 	
-	public void create(Connection conn, CorsoCorsista cc) {
-		
+	public void create(Connection conn, CorsoCorsista cc) throws SQLException {
+		rowSet.setCommand(SELECT_CORSO_CORSISTA);
+		rowSet.execute(conn);
+		rowSet.moveToInsertRow();
+		rowSet.updateLong(1, cc.getCodCorso());
+		rowSet.updateLong(2, cc.getCodCorsista());
+		rowSet.insertRow();
+		rowSet.moveToCurrentRow();
+		rowSet.acceptChanges();
 	}
 }
