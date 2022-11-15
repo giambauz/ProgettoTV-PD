a59d19f13dad04372da50504faa2cacb245b7d66
GabMan03 <gabriele.maniaci@gmail.com>
2022-11-15 09:29:29 +0100

    DAOConstants update

--- ProgettoTVPD/src/main/java/com/betacom/trevisopadova/architecture/dao/DAOConstants.java
+++ ProgettoTVPD/src/main/java/com/betacom/trevisopadova/architecture/dao/DAOConstants.java
@@ -6,4 +6,6 @@ public interface DAOConstants {
 	String SELECT_CORSO_CORSISTA="select * from corso_corsista";
 	String SELECT_CORSOSEQ="select corso_seq.nextval from dual";
 	String SELECT_CORSISTASEQ="select corsista_seq from dual";
+	String SELECT_AMMINISTRATORE_BYID="select * from amministratore where codAdmin=?";
+	String DELETE_CORSO="delete from corso where codCorso=?";
 }
