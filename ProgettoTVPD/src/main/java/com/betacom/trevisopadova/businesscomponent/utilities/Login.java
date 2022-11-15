f1f4e1823e2f2702d370b91a39c77327b2691024
Zanck-03 <m.zanchetta7@gmail.com>
2022-11-15 09:51:32 +0100

    fix login

--- ProgettoTVPD/src/main/java/com/betacom/trevisopadova/businesscomponent/utilities/Login.java
+++ ProgettoTVPD/src/main/java/com/betacom/trevisopadova/businesscomponent/utilities/Login.java
@@ -1,15 +1,17 @@
 package com.betacom.trevisopadova.businesscomponent.utilities;
 
 import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.betacom.trevisopadova.architecture.dao.DAOCostants;
+import com.betacom.trevisopadova.architecture.dao.DAOConstants;
 import com.betacom.trevisopadova.architecture.dbaccess.DBAccess;
+import com.betacom.trevisopadova.businesscomponent.model.Amministratore;
 
-public class Login implements DAOCostants {
+public class Login implements DAOConstants {
 
 	private Connection conn;
 	private PreparedStatement ps;
@@ -21,21 +23,26 @@ public class Login implements DAOCostants {
 		
 	}
 
-	public String getAdmministratorePass(String nomeAdmin, String cognomeAdmin) {
+	public Amministratore getAmministratorePass(String codAdmin) {
 		
-		String passAdmin = null;
+		Amministratore amministratore = null;
 		
 		try {
 			
-			ps = conn.prepareStatement(SELECT_AMMINISTRATORE);
+			ps = conn.prepareStatement(SELECT_AMMINISTRATORE_BYID);
 			
-			ps.setString(1, nomeAdmin);
-			ps.setString(2, cognomeAdmin);
+			ps.setString(1, codAdmin);
 			
 			rs = ps.executeQuery();
 			
-			if(rs.next()) 
-				passAdmin = rs.getString(1);
+			if(rs.next()) {
+				
+				amministratore = new Amministratore();
+				amministratore.setCodAdmin(rs.getString(1));
+				amministratore.setNomeAdmin(rs.getString(2));
+				amministratore.setCognomeAdmin(rs.getString(3));
+				
+			}
 			
 			rs.close();
 			ps.close();
@@ -44,7 +51,7 @@ public class Login implements DAOCostants {
 			e.printStackTrace();
 		}
 		
-		return passAdmin;
+		return amministratore;
 		
 	}
 	
