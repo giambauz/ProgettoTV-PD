b4e08b111964c8db67a0974c29f3216298695e4f
Lucas Teixeira <30601380+teixeira-lucas@users.noreply.github.com>
2022-11-15 10:50:41 +0100

    Create Corsista.java

---
+++ ProgettoTVPD/src/main/java/com/betacom/trevisopadova/businesscomponent/model/Corsista.java
@@ -0,0 +1,41 @@
+package com.betacom.trevisopadova.businesscomponent.model;
+
+public class Corsista {
+	private String nomeCorsista;
+	private String cognomeCorsista;
+	private long codCorsista;
+	private int precedentiFormativi;
+	
+	public String getNomeCorsista() {
+		return nomeCorsista;
+	}
+	public void setNomeCorsista(String nomeCorsista) {
+		this.nomeCorsista = nomeCorsista;
+	}
+	public String getCognomeCorsista() {
+		return cognomeCorsista;
+	}
+	public void setCognomeCorsista(String cognomeCorsista) {
+		this.cognomeCorsista = cognomeCorsista;
+	}
+	public long getCodCorsista() {
+		return codCorsista;
+	}
+	public void setCodCorsista(long codCorsista) {
+		this.codCorsista = codCorsista;
+	}
+	public int getPrecedentiFormativi() {
+		return precedentiFormativi;
+	}
+	public void setPrecedentiFormativi(int precedentiFormativi) {
+		this.precedentiFormativi = precedentiFormativi;
+	}
+	
+	@Override
+	public String toString() {
+		return "Corsista [nomeCorsista=" + nomeCorsista + ", cognomeCorsista=" + cognomeCorsista + ", codCorsista="
+				+ codCorsista + ", precedentiFormativi=" + precedentiFormativi + "]";
+	}
+	
+	
+}
