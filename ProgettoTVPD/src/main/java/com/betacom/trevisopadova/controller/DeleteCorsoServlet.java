1dd37e336303e5c6b6018aa6d0560d99885f1a9d
GabMan03 <gabriele.maniaci@gmail.com>
2022-11-20 16:23:28 +0100

    fix

--- ProgettoTVPD/src/main/java/com/betacom/trevisopadova/controller/DeleteCorsoServlet.java
+++ ProgettoTVPD/src/main/java/com/betacom/trevisopadova/controller/DeleteCorsoServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
